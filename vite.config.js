329ebab3c2985f1ca8df8632cd342cda44c44470
Manan Tank <manantankm@gmail.com>
2023-04-06 02:44:34 +0530

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-  },
-  base: "./",
+	plugins: [
+		react(),
+		nodePolyfills({
+			global: true,
+		}),
+	],
+	define: {
+		'process.env': {},
+	},
+	base: './',
 });
